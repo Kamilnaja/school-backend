690ce78d326b7577877012c6e30c300f7a815877
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-07-26 19:50:57 +0200

    adding db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,9 @@
-insert into post(id, title, content) values (1, 'Test post 1', 'Content 1');
+CREATE TABLE rok_szkolny (
+    id INT PRIMARY KEY,
+    rok_szkolny VARCHAR(100) NOT NULL
+);
+
+INSERT INTO rok_szkolny (id, rok_szkolny) VALUES (1, '2019/2020');
+INSERT INTO rok_szkolny (id, rok_szkolny) VALUES (2, '2020/2021');
+INSERT INTO rok_szkolny (id, rok_szkolny) VALUES (3, '2021/2022');
+INSERT INTO rok_szkolny (id, rok_szkolny) VALUES (4, '2022/2023');
