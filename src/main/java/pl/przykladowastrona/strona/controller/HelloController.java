8e0079007eef96b853843025c0704039216dd6b9
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-07-26 18:22:56 +0200

    Working Hello World

--- src/main/java/pl/przykladowastrona/strona/controller/HelloController.java
+++ src/main/java/pl/przykladowastrona/strona/controller/HelloController.java
@@ -1,14 +1,24 @@
 package pl.przykladowastrona.strona.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.przykladowastrona.strona.service.HelloService;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return helloService.hello();
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring boot";
     }
 }
 
